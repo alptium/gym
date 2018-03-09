e527ca026a1dcec45900280c7b3de17f124d12f7
TamaraG13 <36139219+TamaraG13@users.noreply.github.com>
2018-03-09 23:17:28 +0100

    changes

--- alptium-personl-fitness/src/com/alptium/personalfitness/Member.java
+++ alptium-personl-fitness/src/com/alptium/personalfitness/Member.java
@@ -4,15 +4,45 @@ public class Member {
 
 	// Members info
 	
-	private String  firstname;
+	private String firstName;
 	private String lastName;
 	private int birthYear;
-	private String memberid;
-	private String gender;
+	private int memberId;
+	private char gender;
 	private String address;
-	private boolean hadinjuries;
+	private boolean hasInjuries;
 	
-	public Member(String firstName, String lastName, int birthYear, String memberid, String gender, String address, boolean hadinjuries) {
+	public Member(String firstName, String lastName, int birthYear, int memberId, char gender, String address, boolean hasInjuries) {
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthYear = birthYear;
+		this.memberId = memberId;
+		this.gender = gender;
+		this.address = address;
+		this.hasInjuries = hasInjuries;
+		
+		}
+			public String getFirstName() {
+			return firstName;
+			
+			} public String getLastName() {
+			return lastName;
+		
+			} public int getBirthYear() {
+			return birthYear;
+			
+			} public int getMemberId() {
+			return memberId;
+			
+			} public char getGender() {
+			return gender;
+		
+			} public String getAddress() {
+			return address;
+		
+			} public boolean getHasInjuries() {
+			return hasInjuries;
 		
 	}
 }
