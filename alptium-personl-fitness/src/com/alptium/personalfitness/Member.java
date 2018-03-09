4554640a1476779d8efd330893bc68737d904f58
TamaraG13 <36139219+TamaraG13@users.noreply.github.com>
2018-03-09 23:21:31 +0100

    changes

--- alptium-personl-fitness/src/com/alptium/personalfitness/Member.java
+++ alptium-personl-fitness/src/com/alptium/personalfitness/Member.java
@@ -3,8 +3,7 @@ package com.alptium.personalfitness;
 public class Member {
 
 	// Members info
-	
-<<<<<<< HEAD
+
 	private String firstName;
 	private String lastName;
 	private int birthYear;
@@ -44,17 +43,6 @@ public class Member {
 		
 			} public boolean getHasInjuries() {
 			return hasInjuries;
-=======
-	private String  firstName;
-	private String lastName;
-	private int birthYear;
-	private String memberId;
-	private String gender;
-	private String address;
-	private boolean hasInjuries;
-	
-	public Member(String firstName, String lastName, int birthYear, String memberId, String gender, String address, boolean hasInjuries) {
->>>>>>> fed1765e42933a514fa4a1b89c895c7834633999
 		
 	}
 }
