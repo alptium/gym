f09a25dada25fb83fbca4ee6d86a80d25929bb60
Sasa Joksovic <36681876+SasaOrca@users.noreply.github.com>
2018-03-10 12:28:57 +0100

    Workout

--- alptium-personl-fitness/src/com/alptium/personalfitness/Workout.java
+++ alptium-personl-fitness/src/com/alptium/personalfitness/Workout.java
@@ -2,19 +2,29 @@ package com.alptium.personalfitness;
 
 public class Workout {
 		
-		private String gender;
+		private char gender;
 		private boolean injuries;
+		private String workoutM;
+		private String workoutF;
 		
-		public Workout (String gender, boolean injuries) {
+		public Workout (char gender, boolean injuries, String workoutM, String workoutF) {
 			
 			this.gender = gender;
-			this.injuries = injuries; 
+			this.injuries = injuries;
+			this.workoutM = workoutM;
+			this.workoutF = workoutF;
 			}
-			public String getGendre() {
-				return gender;
+			public char getGendre() {
+					return gender;
 			}
 			public boolean getInjuries() {
-				return injuries;
+					return injuries;
+			}
+			public String getWorkoutM() {
+					return workoutM;
+			}
+			public String getWorkoutF() {
+					return workoutF;
 			}
 		}
 		
